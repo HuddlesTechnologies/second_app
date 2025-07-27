9011ee87dd344c68e12870bc0092243342f6e824
Hudle Technology <154916135+Hudletech@users.noreply.github.com>
2025-07-27 05:52:50 +0100

    .

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,6 +1,8 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 
+final randomizer = Random();
+
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
 
@@ -16,7 +18,7 @@ class _DiceRollerState extends State<DiceRoller> {
   // It will change the image to a different dice face
   void rollDice() {
     setState(() {
-      currentDiceRoll = Random().nextInt(6) + 1;
+      currentDiceRoll = randomizer.nextInt(6) + 1;
     });
   }
 
